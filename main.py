16c4b82313e50014871f289902e571e8d37d3898
701b <701b.vn14a@gmail.com>
2020-05-23 17:33:08 +0900

    statusRandom

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import time
-import string
 import random
 import threading
 
@@ -15,45 +13,52 @@ class App():
         self.edgeAgent = None
         self.timer = None
 
-        # 태아의 상태별 속성값
-        upDownMaxVariation = 15
-
         # 정상 상태
-        normalHeartRateStandard = random.randint(130, 140)  # 태아의 심박수 기준 (130 ~ 140)에서 랜덤
-        normalHeartRateDeviation = 30  # 태아가 정상 상태일 때 심박수 편차
-        normalHeartRateMaxVariance = 3  # 태아가 정상 상태일 때 초당 심박수 최대 변화량
-        probabilityToNormalStatus = 70  # 태아가 정상 상태로 돌아올 확률 (value / 10000)
-        probabilityToUpDown = 1000  #
+        normalHeartRateStandard = 135  # 태아의 심박수 기준
+        normalHeartRateDeviation = 10  # 태아가 정상 상태일 때 심박수 편차
+        normalHeartRateMaxVariance = 2  # 태아가 정상 상태일 때 초당 심박수 최대 변화량
+        probabilityToNormalStatus = 20  # 태아가 정상 상태로 돌아올 확률 (value / 10000)
 
         # 심박수 불규칙 상태
-        irregularHeartRateDeviation = 40  # 태아의 심박수가 불규칙적일 때 심박수 편차
-        irregularHeartRateMaxVariance = 15  # 태아의 심박수가 불규칙적일 때 초당 심박수 최대 변화량
-        probabilityToIrregularHeartRateStatus = 2  # 태아의 심박수가 불규칙적으로 변하게 될 확률 (value / 10000)
+        irregularHeartRateDeviation = 20  # 태아의 심박수가 불규칙적일 때 심박수 편차
+        irregularHeartRateMaxVariance = 6  # 태아의 심박수가 불규칙적일 때 초당 심박수 최대 변화량
+        probabilityToIrregularHeartRateStatus = 5  # 태아의 심박수가 불규칙적으로 변하게 될 확률 (value / 10000)
 
         # 높은 심박수 상태
-        highHeartRateIncrease = 20  # 태아의 심박수가 높은 상태일 때 심박수 기준 상승치
-        highHeartRateDeviation = 35  # 태아의 심박수가 높은 상태일 때 심박수 편차
-        highHeartRateMaxVariance = 8  # 태아의 심박수가 높은 상태일 때 초당 심박수 최대 변화량
-        probabilityToHighHeartRateStatus = 1  # 태아의 심박수가 높은 상태로 변하게 될 확률 (value / 1000)
-
-        probabilityToPressButton = 220  # 태아의 심박수가 높은 상태일 때 임산부가 버튼을 누를 확률 (value / 10000)
+        highHeartRateIncrease = 25  # 태아의 심박수가 높은 상태일 때 심박수 기준 상승치
+        highHeartRateDeviation = 15  # 태아의 심박수가 높은 상태일 때 심박수 편차
+        highHeartRateMaxVariance = 2.8  # 태아의 심박수가 높은 상태일 때 초당 심박수 최대 변화량
+        probabilityToHighHeartRateStatus = 10  # 태아의 심박수가 높은 상태로 변하게 될 확률 (value / 1000)
 
         # 낮은 심박수 상태
         lowHeartRateDecrease = 20  # 태아의 심박수가 낮은 상태일 때 심박수 기준 하락치
-        lowHeartRateDeviation = 20  # 태아의 심박수가 낮은 상태일 때 심박수 편차
-        lowHeartRateMaxVariance = 2  # 태아의 심박수가 낮은 상태일 때 초당 심박수 최대 변화량
-        probabilityToLowHeartRateStatus = 1  # 태아의 심박수가 낮은 상태로 변하게 될 확률 (value / 10000)
+        lowHeartRateDeviation = 6  # 태아의 심박수가 낮은 상태일 때 심박수 편차
+        lowHeartRateMaxVariance = 1.2  # 태아의 심박수가 낮은 상태일 때 초당 심박수 최대 변화량
+        probabilityToLowHeartRateStatus = 10  # 태아의 심박수가 낮은 상태로 변하게 될 확률 (value / 10000)
 
+        # 상태 변화까지 최소 카운트 (value / 10)초
+        minCountUntilStatusVariation = 900
 
+        # 추세
+        upDownMaxVariation = 15
+        probabilityToUpDown = 100  # 추세가 바뀔 확률
 
-        # 상태 변화까지 최소 카운트
-        minCountUntilStatusVariation = 600
+        # 심박수 급락
+        suddenDropAmount = 10  # 심박수가 급락할 때 그 정도
+        probabilityOfSuddenDrop = 15  # 심박수가 급락할 확률 (value / 10000)
+
+        # 태동 버튼
+        probabilityToPressButton = 500  # 태아의 심박수가 높은 상태일 때 임산부가 버튼을 누를 확률 (value / 10000)
+        minIntervalToPressButton = 150  # 임산부가 버튼을 누르는 최소 간격 (value / 10)초
 
         # 현재 수치
         self.fetalStatus = 'normal'
         self.heartRate = normalHeartRateStandard
+        self.preHeartRate = self.heartRate
         self.count = 0
-        self.upDown = 0
+        self.trend = 0
+        self.buttonCount = 0
+        self.suddenDropCount = 21
 
         print("normal heart rate standard : " + str(normalHeartRateStandard))
 
@@ -98,10 +103,10 @@ class App():
             config.node = nodeConfig
 
             deviceConfig = DeviceConfig(id='Device', name='Device', description='Device', deviceType='Smart Device', retentionPolicyName='')
-            heartRateAnalogConfig = AnalogTagConfig(name='heart rate', description='heart rate', readOnly=False, arraySize=0, spanHigh=1000, spanLow=0, engineerUnit='', integerDisplayFormat=4, fractionDisplayFormat=0)
-            fetalMovementAnalogConfig = AnalogTagConfig(name='fetal movement', description='fetal movement', readOnly=False, arraySize=0, spanHigh=1000, spanLow=0, engineerUnit='', integerDisplayFormat=4, fractionDisplayFormat=0)
-            uterineContractionAnalogConfig = AnalogTagConfig(name='uterine contraction', description='uterine contraction', readOnly=False, arraySize=0, spanHigh=1000, spanLow=0, engineerUnit='', integerDisplayFormat=4, fractionDisplayFormat=0)
-            fetalMovementButtonPressedAnalogConfig = AnalogTagConfig(name='fetal movement pressed', description='fetal movement pressed', readOnly=False, arraySize=0, spanHigh=1000, spanLow=0, engineerUnit='', integerDisplayFormat=4, fractionDisplayFormat=0)
+            heartRateAnalogConfig = AnalogTagConfig(name='heart rate', description='heart rate', readOnly=False, arraySize=0, spanHigh=1000, spanLow=0, engineerUnit='', integerDisplayFormat=4, fractionDisplayFormat=1)
+            fetalMovementAnalogConfig = AnalogTagConfig(name='fetal movement', description='fetal movement', readOnly=False, arraySize=0, spanHigh=1000, spanLow=0, engineerUnit='', integerDisplayFormat=4, fractionDisplayFormat=1)
+            uterineContractionAnalogConfig = AnalogTagConfig(name='uterine contraction', description='uterine contraction', readOnly=False, arraySize=0, spanHigh=1000, spanLow=0, engineerUnit='', integerDisplayFormat=4, fractionDisplayFormat=1)
+            fetalMovementButtonPressedAnalogConfig = AnalogTagConfig(name='fetal movement pressed', description='fetal movement pressed', readOnly=False, arraySize=0, spanHigh=1000, spanLow=0, engineerUnit='', integerDisplayFormat=4, fractionDisplayFormat=1)
 
             deviceConfig.analogTagList.append(heartRateAnalogConfig)
             deviceConfig.analogTagList.append(fetalMovementAnalogConfig)
@@ -116,6 +121,7 @@ class App():
             chance = random.randint(1, 10000)
 
             self.count += 1
+            self.buttonCount += 1
 
             if self.count >= minCountUntilStatusVariation:
                 if self.fetalStatus == 'normal':  # 태아가 정상 상태일 때
@@ -123,91 +129,121 @@ class App():
                         self.fetalStatus = 'irregular heart rate'
                         self.count = 0
 
-                    elif self.heartRate > normalHeartRateStandard:  # 태아의 심박수가 높은 편일 때
-                        if 100 < chance <= probabilityToHighHeartRateStatus + 100:
+                    elif self.heartRate > normalHeartRateStandard + normalHeartRateDeviation:  # 태아의 심박수가 높은 편일 때
+                        if 2000 < chance <= probabilityToHighHeartRateStatus + 2000:
                             self.fetalStatus = 'high heart rate'
                             self.count = 0
 
-                    elif self.heartRate < normalHeartRateStandard:  # 태아의 심박수가 낮은 편일 때
-                        if 200 < chance <= probabilityToLowHeartRateStatus + 200:
+                    elif self.heartRate < normalHeartRateStandard - normalHeartRateDeviation:  # 태아의 심박수가 낮은 편일 때
+                        if 4000 < chance <= probabilityToLowHeartRateStatus + 4000:
                             self.fetalStatus = 'low heart rate'
                             self.count = 0
 
                 else:  # 태아가 정상 상태가 아닐 때
                     if chance <= probabilityToNormalStatus:
                         if self.fetalStatus == 'irregular heart rate':  # 불규칙 상태에서 정상으로 돌아올 때 바로 정상 수치 주변으로 돌아오도록 함
-                            self.heartRate = normalHeartRateStandard + random.randint(-normalHeartRateDeviation, normalHeartRateDeviation)
+                            self.heartRate = normalHeartRateStandard + self.trend
 
                         self.fetalStatus = 'normal'
                         self.count = 0
 
             chance = random.randint(1, 10000)
 
-            if chance <= probabilityToUpDown:
-                self.upDown = random.randint(-upDownMaxVariation, upDownMaxVariation)
+            if chance <= probabilityToUpDown and self.fetalStatus != 'irregular heart rate':
+                self.trend += random.randint(-upDownMaxVariation / 3, upDownMaxVariation / 3)
+                if self.trend > upDownMaxVariation:
+                    self.trend = upDownMaxVariation
+                elif self.trend < -upDownMaxVariation:
+                    self.trend = -upDownMaxVariation
 
             def genHeartRateData():
+                power = 8
+                standard = self.trend;
+                maxVariation = 0;
+                deviation = self.trend;
+                restorationProbabilityInRange = 0
+                restorationProbabilityOutRange = 0
+                decreaseBonus = 0
+                increaseBonus = 0
+
+                chance = random.randint(1, 10000)
+
                 if self.fetalStatus == 'normal':
-                    if normalHeartRateStandard + self.upDown + normalHeartRateDeviation >= self.heartRate >= normalHeartRateStandard + self.upDown - normalHeartRateDeviation:
-                        if normalHeartRateStandard + self.upDown < self.heartRate:
-                            self.heartRate += random.randint(-normalHeartRateMaxVariance, int(normalHeartRateMaxVariance / 2))
-                        else:
-                            self.heartRate += random.randint(int(-normalHeartRateMaxVariance / 2), normalHeartRateMaxVariance)
+                    standard += normalHeartRateStandard
+                    maxVariation = normalHeartRateMaxVariance
+                    deviation += normalHeartRateDeviation
+                    restorationProbabilityInRange = 5100
+                    restorationProbabilityOutRange = 6000
 
-                    elif self.heartRate > normalHeartRateStandard + self.upDown + normalHeartRateDeviation:
-                        self.heartRate -= random.randint(int(-normalHeartRateMaxVariance / 2), normalHeartRateMaxVariance)
-
+                    if self.preHeartRate > self.heartRate:
+                        increaseBonus = 2000
                     else:
-                        self.heartRate += random.randint(int(-normalHeartRateMaxVariance / 2), normalHeartRateMaxVariance)
+                        decreaseBonus = 2000
 
                 elif self.fetalStatus == 'irregular heart rate':
-                    if normalHeartRateStandard + self.upDown + irregularHeartRateDeviation >= self.heartRate >= normalHeartRateStandard + self.upDown - irregularHeartRateDeviation:
-                        if normalHeartRateStandard + self.upDown < self.heartRate:
-                            self.heartRate += random.randint(-irregularHeartRateMaxVariance, int(irregularHeartRateMaxVariance / 2))
-                        else:
-                            self.heartRate += random.randint(int(-irregularHeartRateMaxVariance / 2), irregularHeartRateMaxVariance)
-
-                    elif self.heartRate > normalHeartRateStandard + self.upDown + irregularHeartRateDeviation:
-                        self.heartRate -= random.randint(0, irregularHeartRateMaxVariance)
-
-                    else:
-                        self.heartRate += random.randint(0, irregularHeartRateMaxVariance)
+                    standard += normalHeartRateStandard
+                    maxVariation = irregularHeartRateMaxVariance
+                    deviation += irregularHeartRateDeviation
+                    restorationProbabilityInRange = 7000
+                    restorationProbabilityOutRange = 10000
 
                 elif self.fetalStatus == 'high heart rate':
-                    if normalHeartRateStandard + self.upDown + highHeartRateIncrease + highHeartRateDeviation >= self.heartRate >= normalHeartRateStandard + self.upDown + highHeartRateIncrease - highHeartRateDeviation:
-                        if normalHeartRateStandard + self.upDown + highHeartRateIncrease < self.heartRate:
-                            self.heartRate += random.randint(-highHeartRateMaxVariance, int(highHeartRateMaxVariance / 2))
-                        else:
-                            self.heartRate += random.randint(int(-highHeartRateMaxVariance / 2), highHeartRateMaxVariance)
-
-                    elif self.heartRate > normalHeartRateStandard + self.upDown + highHeartRateIncrease + highHeartRateDeviation:
-                        self.heartRate -= random.randint(0, highHeartRateMaxVariance)
-
-                    else:
-                        self.heartRate += random.randint(0, highHeartRateMaxVariance)
+                    standard += normalHeartRateStandard + highHeartRateIncrease
+                    maxVariation = highHeartRateMaxVariance
+                    deviation += highHeartRateDeviation
+                    restorationProbabilityInRange = 5100
+                    restorationProbabilityOutRange = 6000
 
                 elif self.fetalStatus == 'low heart rate':
-                    if normalHeartRateStandard + self.upDown - lowHeartRateDecrease + lowHeartRateDeviation >= self.heartRate >= normalHeartRateStandard + self.upDown - lowHeartRateDecrease - lowHeartRateDeviation:
-                        if normalHeartRateStandard + self.upDown - lowHeartRateDecrease < self.heartRate:
-                            self.heartRate += random.randint(-lowHeartRateMaxVariance, int(lowHeartRateMaxVariance / 2))
+                    standard += normalHeartRateStandard - lowHeartRateDecrease
+                    maxVariation = lowHeartRateMaxVariance
+                    deviation += lowHeartRateDeviation
+                    restorationProbabilityInRange = 5100
+                    restorationProbabilityOutRange = 6000
+
+                # sudden drop인 경우 복원하기
+                if self.suddenDropCount <= 20:
+                    self.heartRate += (suddenDropAmount / 5) * pow(suddenDropAmount, (-self.suddenDropCount / 10))
+                    self.suddenDropCount += 1
+
+                # 범위에 있을 때 확률적으로 sudden drop
+                if standard + deviation >= self.heartRate >= standard - deviation:
+                    if chance <= probabilityOfSuddenDrop and self.suddenDropCount > 20:
+                        self.heartRate -= suddenDropAmount
+                        self.suddenDropCount = 0
+                        # print("sudden drop!")
+
+                    if standard > self.heartRate:
+                        if chance <= restorationProbabilityInRange + increaseBonus:
+                            self.heartRate += pow(random.uniform(0, 1), power) * maxVariation
                         else:
-                            self.heartRate += random.randint(int(-lowHeartRateMaxVariance / 2), lowHeartRateMaxVariance)
-
-                    elif self.heartRate > normalHeartRateStandard + self.upDown - lowHeartRateDecrease + lowHeartRateDeviation:
-                        self.heartRate -= random.randint(0, lowHeartRateMaxVariance)
-
+                            self.heartRate -= pow(random.uniform(0, 1), power) * maxVariation
                     else:
-                        self.heartRate += random.randint(0, lowHeartRateMaxVariance)
+                        if chance <= restorationProbabilityInRange + decreaseBonus:
+                            self.heartRate -= pow(random.uniform(0, 1), power) * maxVariation
+                        else:
+                            self.heartRate += pow(random.uniform(0, 1), power) * maxVariation
+                elif self.heartRate > standard + deviation:
+                    if chance <= restorationProbabilityOutRange + decreaseBonus:
+                        self.heartRate -= pow(random.uniform(0, 1), power) * maxVariation
+                    else:
+                        self.heartRate += pow(random.uniform(0, 1), power) * maxVariation
+                else:
+                    if chance <= restorationProbabilityOutRange + increaseBonus:
+                        self.heartRate += pow(random.uniform(0, 1), power) * maxVariation
+                    else:
+                        self.heartRate -= pow(random.uniform(0, 1), power) * maxVariation
 
                 tag = EdgeTag('Device', 'heart rate', self.heartRate)
                 edgeData.tagList.append(tag)
-                print("generate heart rate data : " + str(self.heartRate) + " (" + self.fetalStatus + ")")
+                # print("generate heart rate data : " + str(self.heartRate) + " (" + self.fetalStatus + "), (" + str(standard) + ")")
 
             def genButtonPressedData():
-                if chance <= probabilityToPressButton and self.fetalStatus == 'high heart rate' and self.count > 10:
+                if chance <= probabilityToPressButton and self.fetalStatus != 'irregular heart rate' and self.heartRate > 160 and self.buttonCount > minIntervalToPressButton:
                     tag = EdgeTag('Device', 'fetal movement pressed', 100)
                     edgeData.tagList.append(tag)
-                    print("generate button pressed data")
+                    # print("generate button pressed data")
+                    self.buttonCount = 0
 
             genHeartRateData()
             genButtonPressedData()
@@ -218,16 +254,20 @@ class App():
             data = generateData()
             self.edgeAgent.sendData(data)
 
-        connect()
+        def start():
+            connect()
 
-        _config = generateConfig()
-        self.edgeAgent.uploadConfig(action=constant.ActionType['Create'], edgeConfig=_config)
+            _config = generateConfig()
+            self.edgeAgent.uploadConfig(action=constant.ActionType['Create'], edgeConfig=_config)
 
-        frequency = 0.1
+            frequency = 0.1
 
-        if self.timer is None:
-            self.timer = RepeatedTimer(frequency, sendData)
-            sendData()
+            if self.timer is None:
+                self.timer = RepeatedTimer(frequency, sendData)
+                sendData()
+
+        t = threading.Thread(target=start)
+        t.start()
 
 
 main = App()
